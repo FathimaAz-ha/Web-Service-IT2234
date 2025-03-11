ff95b2300b27c5e089413335eb117a55ebd57106
FathimaAz-ha <2021ict48@stu.vau.ac.lk>
2025-03-11 14:56:09 +0530

    Created Day-01

---
+++ Day-01/ArithmeticOperation.js
@@ -0,0 +1,16 @@
+let num1 = 56
+let num2 = 65
+let sum = num1 + num2
+let sub = num2 - num1
+let mul = num1 * num2
+let div = num2 / num1			
+
+console.log(sum)	
+console.log(sub)
+console.log(mul)
+console.log(div)
+
+//let Vs var Vs const keywords
+/* let doesn't let duplicate the variable
+while var does let dublicate the variable
+const does not let change the value once it is initialized */
