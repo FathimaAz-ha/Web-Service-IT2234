730dad0e2461249a50bc9c2cbcedba8ee10556b3
Fathima Azha <2021ict48@stu.vau.ac.lk>
2025-06-10 11:14:23 +0530

    Update Project.js

--- Day-16_Day-17/models/Project.js
+++ Day-16_Day-17/models/Project.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose')
+const projectSchema= new mongoose.Schema({
+    pro_id:{type:String,require:true},
+    name:{type:String,require:true}
+})
 
+const Project=mongoose.model('projects',projectSchema)
+const Pr_1=new Project({
+    pro_id:"PR101",
+    name:"Develop a mobile app"
+})
+//Pr_1.save()
+module.exports=Project
