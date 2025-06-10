e1d6167acd9bf4d2eefacd189d73850a3bc4ca23
Fathima Azha <2021ict48@stu.vau.ac.lk>
2025-06-10 11:16:14 +0530

    Create etfRoute.js

---
+++ Day-16_Day-17/route/etfRoute.js
@@ -0,0 +1,37 @@
+const express =require('express')
+const router =express.Router()
+const  Etf= require('../models/Etf')
+const { default:mongoose } = require('mongoose')
+
+router.get('/',async(req,res)=>{
+    try {
+        const results=await Etf.find()
+        if (results) {
+            res.status(200).json(results)
+        } else {
+            res.status(404).send("Sorry,No Data Found !")
+        }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Server Error !")
+        }
+})
+
+router.post('/etfs', async (req, res) => {
+  try {
+    const result = await Etf.insertMany(req.body);
+    res.status(201).json({ message: 'ETF records inserted', result });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get('/etfsWithEmployee', async (req, res) => {
+  try {
+    const etfData = await Etf.find().populate('employeeID'); // Pull employee info using ref
+    res.json(etfData);
+  } catch (err) {
+    res.status(500).json({ error: 'Error fetching ETF data' });
+  }
+});
+module.exports=router
