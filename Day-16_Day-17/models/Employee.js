81b7a99d448ac0b2bf1fc0f8330704f914ffeab9
Fathima Azha <2021ict48@stu.vau.ac.lk>
2025-06-10 11:13:37 +0530

    Update Employee.js

--- Day-16_Day-17/models/Employee.js
+++ Day-16_Day-17/models/Employee.js
@@ -1,1 +1,22 @@
+const mongoose=require('mongoose')
+const employeeSchema=new mongoose.Schema({
+     _id:{type:String,require:true},
+    name:{type:String,require:true},
+    gender:{type:String},
+    EtfID:{type:String,require:true,ref:'etfs'},
+    Dep_ID:{type:String,require:true,ref:'departments'},
+    Pro_ID:[{type:mongoose.Types.ObjectId,ref:'projects'}]
+})
 
+const Employee=mongoose.model('employees',employeeSchema)
+const John=new Employee({
+    _id:'EMP25001',
+    name:'John Doe',
+    gender:'Male',
+    EtfID:"ET001",
+    Dep_ID:"Dep003",
+    Pro_ID:['6833dd0bb8c4ad878d7a4b14']
+})
+
+//John.save()
+module.exports=Employee
